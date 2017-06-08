9bcafc3bff014e4ce10c5c55304c08895fa41b24
Divya Modi <modidivya16@gmail.com>
2017-06-07 23:14:19 -0800

    Modified src/allusers app

--- src/allusers/admin.py
+++ src/allusers/admin.py
@@ -3,7 +3,7 @@ from .models import AllUser
 
 # Register your models here.
 class AllUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email_address', 'first_name', 'last_name']
+    list_display = ['id', 'username', 'email_address', 'first_name', 'last_name']
     list_display_links = ['email_address']
     list_editable = ['username']
     search_fields = ['username', 'email_address']

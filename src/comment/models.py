6dce8cec73f4c676d83c2d1f48d8d3fd0797a437
Divya Modi <modidivya16@gmail.com>
2017-06-07 19:22:12 -0800

    Modified scr/comment/models.py and migrations

--- src/comment/models.py
+++ src/comment/models.py
@@ -3,13 +3,17 @@ from django.conf import settings
 
 # Create your models here.
 def get_sentinel_user():
-    return get_user_model().objects.get_or_create(username='deleted')[0]
+    return get_user_model().objects.get_or_create(username='Unknown')[0]
 
 class Comment(models.Model):
+    post = models.ForeignKey(
+        settings.COMMENTS_BASED_MODEL,
+        on_delete=models.CASCADE,
+    )
     user = models.ForeignKey(
-        settings.CUSTOMIZE_MODEL,
+        settings.COMMENTS_USER_MODEL,
         on_delete=models.SET(get_sentinel_user),
     )
     content   = models.TextField(blank=False, null=False)
-    timestamp = models.DateTimeField(auto_now_add=False)
-    updated   = models.DateTimeField(auto_now=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated   = models.DateTimeField(auto_now=True)

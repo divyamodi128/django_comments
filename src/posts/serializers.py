c40f7b216d1ebe14332acd2f4aa8df819053258a
Divya Modi <modidivya16@gmail.com>
2017-06-12 14:49:51 -0700

    Create serializers.py

--- src/posts/serializers.py
+++ src/posts/serializers.py
@@ -5,9 +5,3 @@ class PostSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
         fields = ('url', 'title', 'content', 'timestamp', 'updated', 'media_url')
-
-'''4703635512 
-ygnesha
-
-
-ranga 510 771 9301 '''

70c0e123de8a909ecf4d4ae64d9324498ccf6578
Divya Modi <modidivya16@gmail.com>
2017-06-17 23:09:09 -0800

    First try using django templates

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics, mixins, permissions
 from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
+# from rest_framework import renderers
+
 from collections import OrderedDict
 
 from .models import Post
@@ -19,10 +21,12 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializers
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
                         #   IsOwnerOrReadOnly,)
+    # For Rendering Template using HTMLRenderer
+    # renderer_classes = (renderers.JSONRenderer, renderers.TemplateHTMLRenderer)
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-    
+
     @detail_route(methods=['get'])
     def get_comments(self, request, pk=None):
         queryset = Post.objects.get(id=pk)
